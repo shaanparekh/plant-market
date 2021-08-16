bb87470cd5e9ab43b9d9fddbaf580dba9d179e28
Shaan Parekh <shaanparekh@Shaans-MacBook-Pro.local>
2021-08-16 14:08:59 -0500

    Login

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAMhhHCt8VAD1g7nErGHtboYcyc8i1c9yM",
+    authDomain: "seedswap-ce25d.firebaseapp.com",
+    projectId: "seedswap-ce25d",
+    storageBucket: "seedswap-ce25d.appspot.com",
+    messagingSenderId: "231665066178",
+    appId: "1:231665066178:web:130fd5cf65ca8fd3f41b5b",
+    measurementId: "G-05YKB9WPG8"
+  };
